b2f55b3f18a8aa55c60ee664a11f714aaaa7e2a8
Bruno Brustoloni <b@b.com>
2025-01-07 14:19:17 +0000

    feat (:core:ui): add octocat place holder

--- core/ui/src/main/kotlin/com/bbrustol/core/ui/utils/LoadImage.kt
+++ core/ui/src/main/kotlin/com/bbrustol/core/ui/utils/LoadImage.kt
@@ -12,7 +12,7 @@ import androidx.compose.ui.res.painterResource
 import coil.compose.SubcomposeAsyncImage
 import coil.request.CachePolicy
 import coil.request.ImageRequest
-import com.bbrustol.ui.R
+import com.bbrustol.core.ui.R
 
 @Composable
 fun LoadImage(
@@ -24,7 +24,7 @@ fun LoadImage(
 ) {
     if (LocalInspectionMode.current) {
         Image(
-            painter = painterResource(id = R.drawable.bg_pokeball),
+            painter = painterResource(id = R.drawable.octocat),
             contentDescription = null,
             contentScale = contentScale,
             modifier = modifier
